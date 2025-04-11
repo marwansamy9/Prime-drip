b31050317fe43ff231484d8d919afb9e4f66f449
marwansamy9 <thriftsplug.eg@gmail.com>
2025-04-11 14:53:18 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Prime Drip</h1>
+      <p>Egyptian-INSPIRED, globally ADMIRED</p>
+      <p>Coming Soon - Your Egyptian-inspired clothing brand</p>
+    </div>
+  );
+}
